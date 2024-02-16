15805507dc4c75246d54035fcf2649609d94b742
Darana <abdullahsaud03@gmail.com>
2024-02-16 05:34:45 +0500

    setup async storage

---
+++ utils/asyncStorage.ts
@@ -0,0 +1,38 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const storeStringData = async (key: string, value: string) => {
+  try {
+    await AsyncStorage.setItem(key, value);
+  } catch (e) {
+    // saving error
+  }
+};
+
+export const storeJSONData = async (key: string, value: string) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(key, jsonValue);
+  } catch (e) {
+    // saving error
+  }
+};
+
+export const getStringData = async (key: string) => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    if (value !== null) {
+      // value previously stored
+    }
+  } catch (e) {
+    // error reading value
+  }
+};
+
+export const getJSONData = async (key: string) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    // error reading value
+  }
+};
