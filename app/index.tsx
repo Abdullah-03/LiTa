8373133288f4ae4fedaaf268a76d08bae67f9068
Abdullah Saud <43924652+Abdullah-03@users.noreply.github.com>
2024-04-20 18:45:03 +0500

    use sqlite rather than asyncstorage

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 //@ts-nocheck
 import { useEffect, useState } from "react";
 import { FlashList } from "@shopify/flash-list";
-import { Frown, Laugh } from "@tamagui/lucide-icons";
+import { Frown, Laugh, Rss } from "@tamagui/lucide-icons";
 import { useRouter } from "expo-router";
 import { Button, H1, Paragraph, Text, XStack, YStack } from "tamagui";
 
@@ -9,12 +9,8 @@ import { MySafeAreaView } from "../components/MySafeAreaView";
 import { MyStack } from "../components/MyStack";
 import NewTrackModal from "../components/NewTrackModal";
 import Track from "../components/track";
-import {
-  getJSONData,
-  getStringData,
-  storeJSONData,
-  storeStringData
-} from "../utils/asyncStorage";
+import db from "../utils/db";
+import { ResultSet } from "expo-sqlite";
 
 interface resultInterface {
   title: string;
@@ -22,28 +18,30 @@ interface resultInterface {
 }
 [];
 
-const today = new Date();
-const year = today.getFullYear();
-const mon = today.getMonth() + 1;
-const day = today.getDate();
-const date = day + "-" + mon + "-" + year;
+// const today = new Date();
+// const year = today.getFullYear();
+// const mon = today.getMonth() + 1;
+// const day = today.getDate();
+// const date = day + "-" + mon + "-" + year;
 
 export default function Home() {
   const [mood, setMood] = useState(-1);
+  const [firstRun, setFirstRun] = useState(true);
 
   console.log("rerender");
 
   function addNewTrack(name: string) {
-    const deepCopyData = JSON.parse(JSON.stringify(data));
-    deepCopyData.push({ title: name, value: 0 });
-    setData(deepCopyData);
-    storeJSONData(
-      date,
-      deepCopyData.reduce(
-        (obj, item) => ((obj[item.title] = item.value), obj),
-        {}
-      )
-    ).then(() => console.log("stored data", deepCopyData));
+    // const deepCopyData = JSON.parse(JSON.stringify(data));
+    // deepCopyData.push({ title: name, value: 0 });
+    // setData(deepCopyData);
+    // storeJSONData(
+    //   date,
+    //   deepCopyData.reduce(
+    //     (obj, item) => ((obj[item.title] = item.value), obj),
+    //     {}
+    //   )
+    // ).then(() => console.log("stored data", deepCopyData));
+    console.log("store track");
   }
 
   const [data, setData] = useState([
@@ -58,44 +56,102 @@ export default function Home() {
   ]);
 
   function deleteTrack(index: number) {
-    const deepCopyData = JSON.parse(JSON.stringify(data));
-    deepCopyData.splice(index, 1);
-    setData(deepCopyData);
-    storeJSONData(
-      date,
-      data.reduce((obj, item) => ((obj[item.title] = item.value), obj), {})
-    ).then(() => console.log("stored data", deepCopyData));
+    console.log("delete track");
+
+    // const deepCopyData = JSON.parse(JSON.stringify(data));
+    // deepCopyData.splice(index, 1);
+    // setData(deepCopyData);
+    // storeJSONData(
+    //   date,
+    //   data.reduce((obj, item) => ((obj[item.title] = item.value), obj), {})
+    // ).then(() => console.log("stored data", deepCopyData));
   }
 
   function onClick(index) {
-    return (value: number) => {
-      const deepCopyData = JSON.parse(JSON.stringify(data));
-      deepCopyData[index].value = value;
-      setData(deepCopyData);
-      storeJSONData(
-        date,
-        data.reduce((obj, item) => ((obj[item.title] = item.value), obj), {})
-      ).then(() => console.log("stored data", deepCopyData));
-    };
+    // return (value: number) => {
+    //   const deepCopyData = JSON.parse(JSON.stringify(data));
+    //   deepCopyData[index].value = value;
+    //   setData(deepCopyData);
+    //   storeJSONData(
+    //     date,
+    //     data.reduce((obj, item) => ((obj[item.title] = item.value), obj), {})
+    //   ).then(() => console.log("stored data", deepCopyData));
+    // };
   }
 
   function moodClick(m: number): void {
-    mood === m ? setMood(-1) : setMood(m);
-    storeStringData(date + " mood", String(m));
+    // mood === m ? setMood(-1) : setMood(m);
+    // storeStringData(date + " mood", String(m));
   }
 
   useEffect(() => {
-    getJSONData(date).then((result: resultInterface) => {
-      console.log("useEffect ran", result);
+    async function get_data() {
+      if (firstRun) {
+        await db.transactionAsync(async (tx) => {
+          await tx
+            .executeSqlAsync(
+              "CREATE TABLE IF NOT EXISTS track (date TEXT DEFAULT CURRENT_DATE UNIQUE NOT NULL);"
+            )
+            .catch(() => console.log("track table creation failed"));
+        });
+        setFirstRun(false);
+      }
 
-      setData(
-        Object.entries(result).map(([title, value]) => ({ title, value }))
-      );
-    });
-    getStringData(date + " mood").then((result) => {
-      result === null ? setMood(-1) : setMood(Number(result));
-      console.log("use effect ran", result);
-    });
+      let result;
+      await db.transactionAsync(async (tx) => {
+        result = await tx
+          .executeSqlAsync("SELECT * FROM track WHERE date = DATE('now');")
+          .then((res) => res.rows)
+          .catch(() => console.log("query failed"));
+      });
+
+      if (Object.keys(result).length === 0) {
+        //no data for today
+        await db.transactionAsync(async (tx) => {
+          result = await tx
+            .executeSqlAsync("SELECT * FROM track LIMIT 1")
+            .then((res) => res.rows)
+            .catch(() => console.log("query failed"));
+        });
+        if (Object.keys(result).length === 0) {
+          //new user with no prior entries (show default entry)
+          setData([
+            {
+              title: "Track First Thing",
+              value: -1
+            }
+          ]);
+        } else {
+          // new day with no entries then populate view with users previous entries
+          ans = {};
+          for (const column in result) {
+            ans[column] = -1;
+          }
+          setData(ans);
+        }
+      } else {
+        //there is tracked data today
+        ans = {};
+        for (const column in result) {
+          ans[column] = -1;
+        }
+        setData(ans);
+      }
+    }
+
+    get_data();
+
+    // getJSONData(date).then((result: resultInterface) => {
+    //   console.log("useEffect ran", result);
+
+    //   setData(
+    //     Object.entries(result).map(([title, value]) => ({ title, value }))
+    //   );
+    // });
+    // getStringData(date + " mood").then((result) => {
+    //   result === null ? setMood(-1) : setMood(Number(result));
+    //   console.log("use effect ran", result);
+    // });
   }, []);
 
   const router = useRouter();
---
+++ utils/db.ts
@@ -0,0 +1,5 @@
+import * as SQLite from "expo-sqlite";
+
+const db = SQLite.openDatabase("main");
+
+export default db;
